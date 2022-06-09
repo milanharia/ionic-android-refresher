caab78f29f5ccf1495b6db5b87a3ea563f45c013
milan.haria <milan.haria@morrisonsplc.co.uk>
2022-06-09 11:34:09 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Android Refresher',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
